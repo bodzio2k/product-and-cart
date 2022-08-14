4e5043a579765d0878a31ae65be1889f8a615a27
bodzio2k <bodzio2k@live.com>
2022-08-14 10:35:54 +0200

    Thing done

---
+++ src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+    <div class="splash-container">
+      <div class="splash">
+        <h1>Splendid Food</h1>
+      </div>
+    </div>
+
+    <main class="wrapper">
+
+      <h2>Recommended</h2>
+
+      <div class="recommended">
+
+        <ProductCard v-for="(item, index) in inventory.slice(0, 3)"
+          v-bind:item="item"
+          v-bind:index="index"
+          v-bind:addToCart="addToCart"
+          class="card"
+        />
+
+      </div>
+
+    </main>
+  </div>
+</template>
+
+<script>
+import ProductCard from '@/components/ProductCard.vue';
+
+export default {
+  props: ['inventory', 'addToCart'],
+  components: {
+    ProductCard
+  }
+}
+</script>
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,25 @@
+<template>
+  <main class="wrapper">
+    <h1>Products</h1>
+
+    <div class="card-container">
+      <ProductCard v-for="(item, index) in inventory"
+          v-bind:item="item"
+          v-bind:index="index"
+          v-bind:addToCart="addToCart"
+          class="card"
+        />
+    </div>
+  </main>
+</template>
+
+<script>
+import ProductCard from '@/components/ProductCard.vue';
+
+export default {
+  props: ['inventory', 'addToCart'],
+  components: {
+    ProductCard
+  }
+}
+</script>
